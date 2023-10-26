770f3174ba946906747e266cc3829c54a58f113f
LevittC17 <LeewednesdayC17@protonmail.com>
2023-10-26 18:28:17 +0000

    fixed BaseCaching.MAX_ITEMS = 2 add 10 items check

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 '''MRUCache module'''
 
@@ -7,28 +7,31 @@ from base_caching import BaseCaching
 
 
 class MRUCache(BaseCaching):
-    '''MRUCache inherits from BaseCache and is a
-    caching system'''
+    """ MRUCache inherits from BaseCaching and is a caching system
+    """
 
     def __init__(self):
-        '''Initialize MRUCache'''
+        """ Initialize MRUCache
+        """
         super().__init__()
         self.access_order = []
 
     def put(self, key, item):
-        '''Add an item in the cache'''
+        """ Add an item in the cache
+        """
         if key is not None and item is not None:
             if key in self.cache_data:
                 self.access_order.remove(key)
             elif len(self.cache_data) >= self.MAX_ITEMS:
                 mru_key = self.access_order.pop()
                 del self.cache_data[mru_key]
-                print('DISCARD: {}'.format(mru_key))
+                print("DISCARD: {}".format(mru_key))
             self.cache_data[key] = item
             self.access_order.insert(0, key)
 
     def get(self, key):
-        '''Get an item by key'''
+        """ Get an item by key
+        """
         if key is not None:
             if key in self.cache_data:
                 self.access_order.remove(key)
