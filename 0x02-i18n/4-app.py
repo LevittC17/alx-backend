f845cf8c657cd7d0da8a2e096d18ca3677bc64d9
LevittC17 <LeewednesdayC17@protonmail.com>
2023-11-03 21:41:17 +0000

    Force locale with URL parameter

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,56 +1,42 @@
 #!/usr/bin/env python3
+'''
+Use Babel to get user locale
+'''
 
-"""
-Force locale with URL parameter
-This module allows you to force a specific locale
-using the locale parameter in the URL
-"""
-
+from flask_babel import Babel
 from flask import Flask, render_template, request
-from flask_babel import Babel, _, gettext
 
+# Create the Flask app with a custom template folder
+app = Flask(__name__, template_folder='templates')
 
-app = Flask(__name__)
-
-
-# Configure Babel
+# Initialize Babel extension
 babel = Babel(app)
 
-
-class Config:
-    """
-    Config class for Flask app.
-    This class sets Babel's default locale and timezone.
-    """
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
+# Define supported languages and configure the app
+app.config['LANGUAGES'] = ['en', 'fr']
+app.config['BABEL_DEFAULT_LOCALE'] = 'en'
+app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
 
 
-app.config.from_object(Config)
+# Define a separate function to create the app
+def create_app():
+    return app
 
 
+# Define a route for the home page
+@app.route('/', methods=['GET'], strict_slashes=False)
+def hello_world():
+    return render_template('3-index.html')
+
+
+# Define a separate function to handle locale selection
 @babel.localeselector
 def get_locale():
-    """
-    Determine the best match with supported languages.
-    This function uses request.accept_languages and the 'locale'
-    parameter in the URL to determine the user's preferred language
-    """
-    user_locale = request.args.get('locale')
-    if user_locale and user_locale in app.config['LANGUAGES']:
-        return user_locale
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route('/')
-def index():
-    """
-    Render the index.html template with
-    parametrized page title and header
-    """
-    return render_template('4-index.html')
-
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run()
