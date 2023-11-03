8ef36cb33a434ab37e56585fb66fe52547242bc0
LevittC17 <LeewednesdayC17@protonmail.com>
2023-11-03 22:07:50 +0000

    Mock logging in

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -6,14 +6,27 @@ This module creates a Flask app that mocks user login and
 displays messages based on user login status
 """
 
-from flask_babel import Babel, _, gettext
+
+from flask_babel import Babel
 from flask import Flask, render_template, request, g
+from typing import Union
 
 
 app = Flask(__name__, template_folder='templates')
 
 
+# Initialize the Babel extension
 babel = Babel(app)
+
+
+# Define Babel configuration
+class Config(object):
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
 
 
 # Define a mock user table
@@ -25,34 +38,44 @@ users = {
 }
 
 
-def get_user(user_id):
+def get_user() -> Union[dict, None]:
     """
-    Define a function to get a user based on the user
-    ID or login_as parameter
+    Define a function to get the user from the session
+    based on the login_as parameter
     """
-    user = users.get(user_id)
-    if user:
-        return user
-    login_as = request.args.get('login_as')
-    if login_as:
+    try:
+        login_as = request.args.get('login_as')
         user = users.get(int(login_as))
+    except (ValueError, KeyError):
+        user = None
     return user
 
 
 @app.before_request
 def before_request():
     """
-    Define a before_request function to set the user
-    in the global context
+    Set the user in the global context using the
+    before_request decorator
     """
-    user_id = request.args.get('login_as')
-    g.user = get_user(int(user_id)) if user_id else None
+    user = get_user()
+    g.user = user
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
-def index():
-    """Render html template"""
+def hello_world() -> str:
+    """Route for the home page"""
     return render_template('5-index.html')
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+    Define a function to get the user's locale for translation
+    """
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 if __name__ == '__main__':
