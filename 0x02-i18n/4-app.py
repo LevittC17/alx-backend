4be159f494fce110cbedb28002ec2d0f206985a4
LevittC17 <LeewednesdayC17@protonmail.com>
2023-11-03 21:50:41 +0000

    added documentation

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python3
-'''
-Use Babel to get user locale
-'''
+
+"""
+Force locale with URL parameter
+This module allows you to force a specific locale
+using the locale parameter in the url
+"""
 
 from flask_babel import Babel
 from flask import Flask, render_template, request
 
-# Create the Flask app with a custom template folder
+
 app = Flask(__name__, template_folder='templates')
+
 
 # Initialize Babel extension
 babel = Babel(app)
 
-# Define supported languages and configure the app
 app.config['LANGUAGES'] = ['en', 'fr']
 app.config['BABEL_DEFAULT_LOCALE'] = 'en'
 app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
@@ -20,18 +23,26 @@ app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
 
 # Define a separate function to create the app
 def create_app():
+    """Return route"""
     return app
 
 
 # Define a route for the home page
 @app.route('/', methods=['GET'], strict_slashes=False)
 def hello_world():
+    """Render the html template"""
     return render_template('3-index.html')
 
 
 # Define a separate function to handle locale selection
 @babel.localeselector
 def get_locale():
+    """
+    Determine the best match wot supported language
+    This function uses request.accept_languages and the
+    'locale' parameter in the URL to determine the user's
+    preferred language
+    """
     locale = request.args.get('locale')
     if locale in app.config['LANGUAGES']:
         return locale
